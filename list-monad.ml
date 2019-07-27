0b85a210e047bbe49574c8d34abd2fa655e2de8e
Chiaki ISHIO <chiaki.ishio@gmail.com>
2019-07-27 23:15:56 +0900

    add comments, change encoding from EUC to UTF-8, continuation monad

--- list-monad.ml
+++ list-monad.ml
@@ -1,4 +1,4 @@
-(* int list �����Ǥι�פ��֤� *)
+(* sum functions (sum1, sum2, sum3, sum4) over int list *)
 
 type 'a monad_t = 'a
 
@@ -18,11 +18,10 @@ let rec sum1 lst : int monad_t = match lst with
       bind (sum1 rest) (fun i1 ->
       return (i + i1))
 
-(* �ᥤ�� *)
-
+(* メイン *)
 let main1 lst = run (fun () -> sum1 lst)
 
-(* �ǡ����˥��顼 (0, ��) �������� *)
+(* データにエラー (0, 負) がある場合 *)
 
 type 'a monad_t = Error of string
 		| Success of 'a
@@ -53,23 +52,23 @@ let rec sum2 lst = match lst with
       bind (sum2 rest) (fun i2 ->
       return (i + i2))
 
-(* �ᥤ�� *)
+(* メイン *)
 
 let main2 lst = run (fun () -> sum2 lst)
 
-(* Ʊ�������ϥ�����Ȥ��ʤ� *)
+(* 同じ数字はカウントしない *)
 
 type state_t = int list
 type 'a monad_t = state_t -> 'a * state_t
 
-(* return : 'a -> 'a monad_t *)
+(* return は状態を変更しない *)
 let return (x : 'a) : 'a monad_t =
   fun lst -> (x, lst)
 
 (* bind : 'a monad_t -> ('a -> 'b monad_t) -> 'b monad_t *)
 let bind (x : 'a monad_t) (f : 'a -> 'b monad_t) : 'b monad_t =
-  fun lst -> let (v1, lst1) = x lst in
-	     let (v2, lst2) = f v1 lst1 in
+  fun lst -> let (v1, lst1) = x lst in (* x に現在の状態を渡して実行させる *)
+	     let (v2, lst2) = f v1 lst1 in (* f v1 に最新の状態 lst1 を渡す *)
 	     (v2, lst2)
 
 (* run : (unit -> 'a monad_t) -> 'a *)
@@ -93,11 +92,11 @@ let rec sum3 lst = match lst with
 		bind (sum3 rest) (fun i2 ->
 		return (i + i2))))
 
-(* �ᥤ�� *)
+(* メイン *)
 
 let main3 lst = run (fun () -> sum3 lst)
 
-(* ���䤬ʣ�������� *)
+(* 候補が複数ある場合 *)
 
 type 'a monad_t = 'a list
 
@@ -109,6 +108,8 @@ let bind (x : 'a monad_t) (f : 'a -> 'b monad_t) : 'b monad_t =
   List.concat (List.map f x)
 
 (* run1 : (unit -> 'a monad_t) -> 'a *)
+(* 一つでも解が出てきたらおしまい: run all して最初の一個を返しているだけ
+   もし一つもなかったら Fail してくれる *)
 let run1 (thunk : unit -> 'a monad_t) =
   match thunk () with
     [] -> failwith "No answer found"
@@ -133,7 +134,7 @@ let rec sum4 lst : int monad_t = match lst with
       bind (sum4 rest) (fun i1 ->
       return (i + i1))
 
-(* �ᥤ�� *)
+(* メイン *)
 
 (* ndlist2list : 'a list list -> 'a list monad_t *)
 let rec ndlist2list lst = match lst with
@@ -153,8 +154,74 @@ let main4' lst : 'a list =
 	 bind (ndlist2list lst) (fun lst' ->
 	 sum4 lst'))
 
-(* �� *)
+(* 例 *)
 
 let lst1 = [1; 3; 0; -1; 2; 4; 3]
 
+(* 本当は、このリストは monad_t を直接書き表してしまっているので、できれば
+   抽象データ型で書きたい気持ちがある。しかしリストの中に monad_t を並べることは
+   できないので、正確には bind を用いて書くべき。
+   (なので型は 'a monad_t list ではなく 'a list monad_t がよい。
+    OCaml 的には両方リストで表現されているのでどちらでも通ってしまうけど…)*)
+(* bind (return 0) (fun _ ->
+   bind (choice [3; 4]) (fun _ ->
+   ... )) のような感じ *)
+(* 下のように (簡単のために) 直接リストで書かれたものを bind を用いた形に
+   変換しているのが ndlist2list という関数 *)
 let lst2 = [[1]; [3; 4]; [0]; [-1]; [2]; [4; 6]; [3]]
+
+(* 継続モナド *)
+
+type ('a, 'ans) monad_t = ('a -> 'ans) -> 'ans
+
+(* return : 'a -> ('a, 'ans) monad_t *)
+let return (x : 'a) : ('a, 'ans) monad_t = fun k -> k x
+
+(* bind : ('a, 'ans) monad_t -> ('a -> ('b, 'ans) monad_t) ->
+          ('b, 'ans) monad_t *)
+let bind (x : ('a, 'ans) monad_t) (f : 'a -> ('b, 'ans) monad_t)
+  : ('b, 'ans) monad_t = fun k -> x (fun v -> f v k)
+
+(* callcc : (('a -> ('b, 'ans) monad_t) -> ('a, 'ans) monad_t) ->
+            ('a, 'ans) monad_t *)
+(* パースの法則 *)
+let callcc (f : ('a -> ('b, 'ans) monad_t) -> ('a, 'ans) monad_t)
+  : ('a, 'ans) monad_t = fun k -> f (fun v k' -> k v) k
+
+(* run : (unit -> ('ans, 'ans) monad_t) -> 'ans *)
+let run (thunk : unit -> ('a, 'ans) monad_t) =
+  thunk () (fun x -> x)
+
+(* sum5 : int list -> (int, 'ans) monad_t *)
+let rec sum5 lst : (int, 'ans) monad_t = match lst with
+    [] -> return 0
+  | i :: rest ->
+      bind (sum5 rest) (fun i1 ->
+      return (i + i1))
+
+(* メイン *)
+
+let main5 lst = run (fun () -> sum5 lst)
+
+(* 継続 parameterized モナド *)
+(* ans を固定のままだと s/r にできないので変えられるようにする *)
+type ('a, 's, 't) monad_t = ('a -> 's) -> 't
+
+(* return : 'a -> ('a, 'a, 't) monad_t *)
+let return (x : 'a) : ('a, 's, 's) monad_t = fun k -> k x
+
+(* bind : ('a, 'ans) monad_t -> ('a -> ('b, 'ans) monad_t) ->
+          ('b, 'ans) monad_t *)
+let bind (x : ('a, 't, 'u) monad_t) (f : 'a -> ('b, 's, 't) monad_t)
+  : ('b, 's, 'u) monad_t = fun k -> x (fun v -> f v k)
+
+(* reset : ('a, 'a, 's) monad_t -> ('s, 't, 't) monad_t *)
+let reset (f : ('a, 'a, 's) monad_t) : ('s, 't, 't) monad_t
+  = fun k -> k (f (fun x -> x))
+
+(* shift : (('p -> ('a, 't, 't) monad_t) -> ('s, 's, 'b) monad_t) ->
+           ('p, 'a, 'b) monad_t *)
+let shift (f : ('p -> ('a, 't, 't) monad_t) -> ('s, 's, 'b) monad_t)
+  : ('p, 'a, 'b) monad_t = fun k -> f (fun v k' -> k' (k v)) (fun x -> x)
+(* k' (k v) と書いているので、
+   k' と k が direct style で compose されている *)
--- list.ml
+++ list.ml
@@ -1,13 +1,14 @@
-(* int list の要素の合計を返す *)
+(* sum functions (sum1, sum2, sum3, sum4) over int list *)
+
 (* sum1 : int list -> int *)
 let rec sum1 lst = match lst with
     [] -> 0
   | i :: rest -> i + sum1 rest
 
-(* データにエラー (0, 負) がある場合 *)
-
-type 'a error_t = Error of string
-		| Success of 'a
+(* Error when elements are 0 or negative, Success otherwise *)
+type 'a error_t =
+    Error of string
+  | Success of 'a
 
 (* sum2 : int list -> int error_t *)
 let rec sum2 lst = match lst with
@@ -19,8 +20,7 @@ let rec sum2 lst = match lst with
 	  Error (s) -> Error (s)
 	| Success (i2) -> Success (i + i2)
 
-(* 同じ数字はカウントしない *)
-
+(* keep track of added numbers, only add different numbers *)
 (* sum3 : int list -> int list -> int * int list *)
 let rec sum3 lst history = match lst with
     [] -> (0, history)
@@ -31,8 +31,8 @@ let rec sum3 lst history = match lst with
       else let (i2, history2) = sum3 rest (i :: history) in
 	   (i + i2, history2)
 
-(* 候補が複数ある場合 *)
-
+(* multiple possible answers *)
+(* corresponds to non-deterministic monad *)
 (* sum4 : int list list -> int list *)
 let rec sum4 lst = match lst with
     [] -> [0]
@@ -41,8 +41,12 @@ let rec sum4 lst = match lst with
 	List.map (fun i -> i + i2) lst
       ) lst1)
 
-(* 例 *)
-
+(* examples *)
 let lst1 = [1; 3; 0; -1; 2; 4; 3]
-
 let lst2 = [[1]; [3; 4]; [0]; [-1]; [2]; [4; 6]; [3]]
+
+(* tests *)
+let test1_lst1 = sum1 lst1 = 12
+let test2_lst1 = sum2 lst1 = Error "zero"
+let test3_lst1 = sum3 lst1 [] = (9, [4; 2; -1; 0; 3; 1])
+let test4_lst2 = sum4 lst2 = [12; 13; 14; 15]
